b11208e3234d4ce0215b9a9c48d16d29e461f0a4
willerdasilva <willerdasilva@tamu.edu>
2017-04-25 00:23:37 +0000

    bug testing, commented back in the carrierwave stuff in show profile view

--- app/models/specific_photographer.rb
+++ app/models/specific_photographer.rb
@@ -67,16 +67,18 @@ class SpecificPhotographer < ApplicationRecord
       else
         puts "FINAL STEP - We have experience to search by, but no genre matches previously. Search entire model and check for the params"
         SpecificPhotographer.all.find_each do |user_object|
+          if params_arg[:experience] == user_object[:experience]
+            puts "KLDSJSDLJDKJSLDJKLSDJLKSJDLKSJDLSJDKJSDLJKLSDJLKDJLDKSJLKSD"
+            puts params_arg[:experience]
+            puts user_object[:experience]
+            @return_array.push(user_object[:user_key])
+          end
           #if SpecificPhotographer.where("experience ILIKE ?", params_arg[:experiece])
           #  puts "KLDSJSDLJDKJSLDJKLSDJLKSJDLKSJDLSJDKJSDLJKLSDJLKDJLDKSJLKSD"
           #  puts params_arg[:experience]
+          #  puts user_object[:experience]
           #  @return_array.push(user_object[:user_key])   #Might need to find by instead... very worse in efficiency tbh.
           #end
-          if SpecificPhotographer.where("experience ILIKE ?", params_arg[:experiece])
-            puts "KLDSJSDLJDKJSLDJKLSDJLKSJDLKSJDLSJDKJSDLJKLSDJLKDJLDKSJLKSD"
-            puts params_arg[:experience]
-            @return_array.push(user_object[:user_key])   #Might need to find by instead... very worse in efficiency tbh.
-          end
         end
       end
     else
