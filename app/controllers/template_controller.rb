08ff620a9107a1aeff858dd8a326e5aaee2f6861
Ritu Raj <rituraj131@tamu.edu>
2017-10-20 15:07:19 -0500

    added app bootstrap to new profession

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,6 +1,5 @@
 class TemplateController < ApplicationController
   def create
-    flash[:notice] = "successfully created."
     type = params[:prof_name]
     #attributes1 = params.select { |key, value| key.to_s.match(/^field_*/) }
     field_name_arr = params[:field_name]
@@ -8,16 +7,15 @@ class TemplateController < ApplicationController
     attributes_json = []
     #puts "printing attributes", params[:field_name]
     field_name_arr.each_with_index do |field_name, index|
-      puts "printing key",field_name
-      puts "printing value",field_type_arr[index]
       attributes_json << [field_name: field_name, field_type: field_type_arr[index]]
     end
     attributes_json = attributes_json.to_json
-    puts "attributes1 ", attributes_json
+    puts "attributes_json ", attributes_json
     professional = Template.create(prof_name: type, prof_attribute: attributes_json)
     #params = ActionController::Parameters.new(prof_name: type, attributes: attributes1)
     #Template.new(params.permit(:prof_name,:attributes))
     professionals = Template.all
+    flash[:success] = "New profession successfully created."
   end
 
   # Params used to create the LoginInfo object
