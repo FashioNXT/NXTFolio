b829014e4a66f2b7edc3e31037a5edd909a93f51
Aniket Panse <aniketpanse@gmail.com>
2017-11-05 19:09:22 -0600

    fixed migrations etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105053437) do
+ActiveRecord::Schema.define(version: 20171105234827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 20171105053437) do
   end
 
   create_table "templates", force: :cascade do |t|
-    t.string   "type"
+    t.string   "prof_name"
     t.json     "prof_attribute"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
