d86a0a8cb21fa11eb0cb94e8f4d6d6850e5bf230
Aniket Panse <aniketpanse@gmail.com>
2017-11-15 13:18:55 -0600

    fixed another migration :/

--- db/migrate/20171105234827_rename_type_to_prof_name.rb
+++ db/migrate/20171105234827_rename_type_to_prof_name.rb
@@ -1,5 +1,7 @@
 class RenameTypeToProfName < ActiveRecord::Migration[5.0]
   def change
-    rename_column :templates, :type, :prof_name
+    if column_exists?(:templates, :type)
+      rename_column :templates, :type, :prof_name
+    end
   end
 end
