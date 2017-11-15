31d0e234642e828c0fade5318a7e41f76f188144
Aniket Panse <aniketpanse@gmail.com>
2017-11-15 13:49:01 -0600

    ensure that search results take you to the profile page

--- app/controllers/show_profile_controller.rb
+++ app/controllers/show_profile_controller.rb
@@ -1,36 +1,37 @@
 class ShowProfileController < ApplicationController
   # Associated with the view that displays after a profile is selected from the search results list
   def show_profile
-    if session[:current_user_key] != nil 
-      @error = session[:current_user_key].to_s
-      puts "sessions current_user_key" + session[:current_user_key].to_s
-      
-      if GeneralInfo.exists?(:userKey => session[:current_user_key])
-        @general_info = GeneralInfo.find_by(userKey: session[:current_user_key])
+    if session[:current_user_key] != nil or params[:user_key] != nil
+      user_key_current = session[:current_user_key] || params[:user_key]
+      @error = user_key_current.to_s
+      puts "sessions current_user_key" + user_key_current.to_s
+
+      if GeneralInfo.exists?(:userKey => user_key_current)
+        @general_info = GeneralInfo.find_by(userKey: user_key_current)
         @general_info_attributes = GeneralInfo.attribute_names
         @general_info_values = @general_info.attribute_values
         
         case @general_info.specific_profile_id
         when 1
           @profile_type = "Designer"
-          if SpecificDesigner.exists?(:user_key => session[:current_user_key])
-            @specific_designer = SpecificDesigner.find_by(user_key: session[:current_user_key])
+          if SpecificDesigner.exists?(:user_key => user_key_current)
+            @specific_designer = SpecificDesigner.find_by(user_key: user_key_current)
             @profile_info = @specific_designer.attribute_values
           else
             #stuff
           end
         when 2
           @profile_type = "Model"
-          if SpecificModel.exists?(:user_key => session[:current_user_key])
-            @specific_model = SpecificModel.find_by(user_key: session[:current_user_key])
+          if SpecificModel.exists?(:user_key => user_key_current)
+            @specific_model = SpecificModel.find_by(user_key: user_key_current)
             @profile_info = @specific_model.attribute_values
           else
             #stuff
           end
         when 3
           @profile_type = "Photographer"
-          if SpecificPhotographer.exists?(:user_key => session[:current_user_key])
-            @specific_photographer = SpecificPhotographer.find_by(user_key: session[:current_user_key])
+          if SpecificPhotographer.exists?(:user_key => user_key_current)
+            @specific_photographer = SpecificPhotographer.find_by(user_key: user_key_current)
             @profile_info = @specific_photographer.attribute_values
           else
             #stuff
