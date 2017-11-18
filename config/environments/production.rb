20c963cb1daa1d5d13675aaef88c9c09ad9c0286
avinsaxe <avinsaxe@tamu.edu>
2017-11-18 16:46:48 -0600

    Facebook login changes for login alone. Localhost used as of now. Login is just an authentication. Account getting created without a name. Have to check how can we fetch details from facebook and create a base account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'http://fashion-links.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
