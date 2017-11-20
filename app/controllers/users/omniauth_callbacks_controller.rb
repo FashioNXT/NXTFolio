b469bcef751f500005d613038e330e5edff67279
avinsaxe <avinsaxe@tamu.edu>
2017-11-19 21:19:10 -0600

    Not working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    params[:email]='me@me.com'
+    params[:password]='password'
+    @login_info_controller=LoginInfoController.new
+    @login_info_controller.login
+    @login_info_controller.login_info[:email]='me@me.com'
+    @login_info_controller.login_info[:password]='password'
+    @login_info_controller.login_submit
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
