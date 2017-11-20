14f14cbf18858043652c11aabb49c557345e29b2
Avinash Saxena <avinsaxe@tamu.edu>
2017-11-19 22:41:35 -0600

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    params[:email]='me@me.com'
-    params[:password]='password'
-    @login_info_controller=LoginInfoController.new
-    @login_info_controller.login
-    @login_info_controller.login_info[:email]='me@me.com'
-    @login_info_controller.login_info[:password]='password'
-    @login_info_controller.login_submit
-
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
@@ -16,6 +8,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+    userKey = SecureRandom.hex(10)
+    session[:current_user_key] = userKey
   end
 
   def failure
