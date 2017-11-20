54b81a7714bb70f21b5ce7b203c1502389568265
yachuan815 <yachuan815@tamu.edu>
2017-11-19 18:25:44 -0600

    Update general_info_controller.rb

--- app/controllers/general_info_controller.rb
+++ app/controllers/general_info_controller.rb
@@ -44,8 +44,6 @@ class GeneralInfoController < ApplicationController
     @templates.unshift('Model')
     @templates.unshift('Photographer')
     @general_info = GeneralInfo.new
-    @general_info.first_name = 'e'
-    @general_info.last_name = 'e'
   end
  
 
