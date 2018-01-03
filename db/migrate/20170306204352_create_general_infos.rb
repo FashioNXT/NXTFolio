0ffdd65b64501bf488494131bccb2abe32d54afd
avinsaxe <avinsaxe@tamu.edu>
2018-01-03 02:09:46 -0500

    Migration for phone number generated

--- db/migrate/20170306204352_create_general_infos.rb
+++ db/migrate/20170306204352_create_general_infos.rb
@@ -13,7 +13,6 @@ class CreateGeneralInfos < ActiveRecord::Migration[5.0]
       t.string :country
       t.string :state
       t.string :city
-      t.string :phone
       
       t.string :compensation
       t.string :facebook_link
