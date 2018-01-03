2483a3ae71b368596c4b8ecf892101b118be064f
avinsaxe <avinsaxe@tamu.edu>
2018-01-03 02:16:28 -0500

    Validation for phone number added in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125205853) do
+ActiveRecord::Schema.define(version: 20180103070721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20171125205853) do
     t.string   "userKey"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "phone"
     t.integer  "month_ofbirth"
     t.integer  "day_ofbirth"
     t.integer  "year_ofbirth"
@@ -41,6 +40,7 @@ ActiveRecord::Schema.define(version: 20171125205853) do
     t.string   "gallery_pictures",                  array: true
     t.integer  "template_id"
     t.json     "specific_profile"
+    t.string   "phone"
   end
 
   create_table "login_infos", force: :cascade do |t|
