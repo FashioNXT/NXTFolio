43210eea3b2decd5b3b5a9206d8768718abb0129
Bobby Gould <bgould96@gmail.com>
2018-03-24 21:56:20 -0500

    Added search functionality for specific professions.
    TODO: need to finish testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103070721) do
+ActiveRecord::Schema.define(version: 20180320055730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20180103070721) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "user_key"
+    t.string   "state"
   end
 
   create_table "templates", force: :cascade do |t|
