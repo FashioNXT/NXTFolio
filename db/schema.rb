f34c0d7d947ae0a3d4afc9ff4ad0a1475f5d10c3
Jon Janzen <jjjonjanzen@gmail.com>
2018-04-28 15:53:33 -0500

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,10 @@ ActiveRecord::Schema.define(version: 20180427172826) do
     t.integer  "template_id"
     t.json     "specific_profile"
     t.string   "phone"
-    t.text     "job_attr"
-    t.string   "job_name"
     t.float    "latitude"
     t.float    "longitude"
+    t.text     "job_attr"
+    t.string   "job_name"
     t.boolean  "is_admin"
   end
 
