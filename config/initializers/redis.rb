e39762096b519274e9f3465fc61b4b775af5e5c5
Jonathan Janzen <jjjonjanzen@gmail.com>
2018-05-05 11:03:48 -0500

    Implement a fallback FS mocked version of redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,40 @@
-$redis = Redis.new(url: ENV["REDIS_URL"])
+class MockRedis
+	def initialize()
+		@dirname = "local_redis"
+	end
 
+	def _create_dir()
+		unless File.directory?(@dirname)
+			puts "creating dir"
+			FileUtils.mkdir_p(@dirname)
+		end
+	end
+
+	def set(name, content)
+		_create_dir()
+		File.open(File.join(@dirname, name), 'w') {
+			|file| file.puts content
+		}
+	end
+
+	def del(name)
+		_create_dir()
+		FileUtils.rm(File.join(@dirname, name))
+	end
+
+	def get(name)
+		_create_dir()
+		begin
+			return File.read(File.join(@dirname, name))
+		rescue
+			return ""
+		end
+	end
+end
+
+if ENV["REDIS_URL"].nil? || ENV["REDIS_URL"].empty?
+	$redis = MockRedis.new()
+else
+	$redis = Redis.new(url: ENV["REDIS_URL"])
+end
 
