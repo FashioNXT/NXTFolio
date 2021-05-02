34b3c37127d48ec52794f354e773a5cfa33d2228
Peng <allenhuang818@gmail.com>
2021-05-01 22:20:03 -0500

    revise application.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210428194856) do
+ActiveRecord::Schema.define(version: 20210430184725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
