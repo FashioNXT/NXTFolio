7fcbd070f09b4c983612c56ec7bfda41866266c9
Ahmed Tanvir Mahdad <mahdad@uab.edu>
2022-03-15 01:11:25 -0500

    Another try to deployment

---
+++ public/assets/application-68fa41b9d55ff4e4535ae85f21018bb988db23580870280d66af7ed861e86f54.js
@@ -0,0 +1,87 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $("#Model1").show();
+    $("#Model").addClass("active");
+
+    $("#Photographer1").hide();
+    $("#Agent1").hide();
+    $("#Designer1").hide();
+    $("#Everyone1").hide();
+    $("#Model").click(function () {
+        $("#Model1").show();
+        $("#Photographer1").hide();
+        $("#Agent1").hide();
+        $("#Designer1").hide();
+        $("#Everyone1").hide();
+        $("#Designer").removeClass("active");
+        $("#Everyone").removeClass("active");
+        $("#Agent").removeClass("active");
+        $("#Photographer").removeClass("active");
+        $("#Model").addClass("active");
+    });
+    $("#Photographer").click(function () {
+        $("#Model1").hide();
+        $("#Model").removeClass("active");
+        $("#Designer").removeClass("active");
+        $("#Everyone").removeClass("active");
+        $("#Agent").removeClass("active");
+        $("#Photographer").addClass("active");
+        $("#Photographer1").show();
+        $("#Agent1").hide();
+        $("#Designer1").hide();
+        $("#Everyone1").hide();
+    });
+    $("#Agent").click(function () {
+        $("#Model1").hide();
+        $("#Model").removeClass("active");
+        $("#Designer").removeClass("active");
+        $("#Everyone").removeClass("active");
+        $("#Photographer").removeClass("active");
+        $("#Agent").addClass("active");
+        $("#Photographer1").hide();
+        $("#Agent1").show();
+        $("#Designer1").hide();
+        $("#Everyone1").hide();
+    });
+    $("#Designer").click(function () {
+        $("#Model1").hide();
+        $("#Model").removeClass("active");
+        $("#Photographer").removeClass("active");
+        $("#Agent").removeClass("active");
+        $("#Designer").addClass("active");
+        $("#Everyone").removeClass("active");
+        $("#Photographer1").hide();
+        $("#Agent1").hide();
+        $("#Everyone1").hide();
+        $("#Designer1").show();
+    });
+    $("#Everyone").click(function () {
+        $("#Model1").hide();
+        $("#Model").removeClass("active");
+        $("#Photographer").removeClass("active");
+        $("#Agent").removeClass("active");
+        $("#Designer").removeClass("active");
+        $("#Photographer1").hide();
+        $("#Everyone").addClass("active");
+        $("#Agent1").hide();
+        $("#Designer1").hide();
+        $("#Everyone1").show();
+    });
+    
+});
+
+
+
