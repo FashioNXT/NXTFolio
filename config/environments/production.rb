d634e211af440c9aa9c466f2b424f279f6e3eeef
Ahmed Tanvir Mahdad <mahdad@uab.edu>
2022-03-29 09:24:27 -0500

    Trying to fix static assets in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  #
+  # SErving static assets
+   config.serve_static_assets = true
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
