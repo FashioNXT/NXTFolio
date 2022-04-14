0a6cf2b718e80a8fd8fdf794c6ac8df121596064
Ahmed Tanvir Mahdad <mahdad@uab.edu>
2022-04-14 02:54:26 -0500

    Cucumber test for User story 19 is added

--- features/step_definitions/user_story_19_steps.rb
+++ features/step_definitions/user_story_19_steps.rb
@@ -1,68 +1,102 @@
-Given(/^I am a professional and wrofile$/) do
+Given(/^I am a professional and search for another professional$/) do
   visit root_path
 end
 
-When(/^I click ohome page$/) do
-  click_on "Register Your Business"
+When(/^I wish to click on search button on home page for country and city check$/) do
+  click_on "search_submit"
 end
 
-Then(/^I should be ablee username anto register$/) do
-  fill_in "email", :with => 'fashionxt101@gmail.com'
-  fill_in "password", :with => 'Abc123!'
-  fill_in "confirm", :with => 'Abc123!'
-end
-
-And(/^I should be r tha entry page$/) do
-  click_button "commit"
-  expect(current_path).to include('general_info/new')
-end
-
-
-Given(/^I am a professioto enter personal data$/) do
-  visit root_path
-  click_on "Register Your Business"
-  fill_in "email", :with => 'fashionxt102@gmail.com'
-  fill_in "password", :with => 'Abc123!'
-  fill_in "confirm", :with => 'Abc123!'
-  click_button "commit"
-  expect(current_path).to include('general_info/new')
-end
-
-When(/^I submitted all thpersonal information$/) do
-  fill_in "first_name", :with => 'Test'
-  fill_in "last_name", :with => 'User'
-  fill_in "company", :with => 'Test Company'
-  select 'Creators', from: 'general_info_industry'
-  select 'Designer', from: 'general_info_job_name'
-  fill_in "highlights", :with => 'highlights'
+Then(/^I should be able to provide country and city information on that page$/) do
   select 'United States', from: 'country'
   select 'Texas', from: 'state'
   select 'College Station', from: 'city'
-  fill_in "emailaddr", :with => 'fashionxt@gmail.com'
-  #sleep(inspection_time=80)
-  click_button('SAVE AND CONTINUE')
-  #sleep(inspection_time=80)
-  #expect(page).to have_button('SAVE and GO TO PROFILE')
 end
 
-Then(/^After saveional detail page and provide information and submit$/) do
+And(/^after clicking search, I can again provide country and city information on that page$/) do
+  click_on "show_page_button"
+  select 'United States', from: 'country'
+  select 'Texas', from: 'state'
+  select 'College Station', from: 'city'
 
-  expect(page).to have_button('SAVE and GO TO PROFILE')
-  fill_in "specialization", :with => 'my specialization'
-  fill_in "profdetails", :with => 'ProfessionalDetails'
-  fill_in "bio", :with => 'About me'
-  fill_in "general_info_facebook_link", :with => 'My facbook profile'
-  fill_in "general_info_facebook_link", :with => 'My linkedin profile'
-  fill_in "general_info_facebook_link", :with => 'My site address'
-  select 'Salary', from: 'general_info_compensation'
-  fill_in "experience", :with => '2'
-  click_button "select_two"
-  sleep(inspection_time=20)
 end
 
-And(/^I will be rediretail page$/) do
-  expect(current_path).to include('show_profile')
+Given(/^I am a professional and intend to click the search button$/) do
+  visit root_path
 end
+
+When(/^I intend to click the search button on home page$/) do
+  click_on "search_submit"
+end
+
+Then(/^Default country option united states should be selected$/) do
+  sleep(inspection_time=5)
+  expect(page).to have_select(
+                    'country',         # locator
+                    selected: 'United States' # option
+                  )
+end
+
+And(/^After submitting this search page, I can again see united states as the default contry in search result page$/) do
+  click_on "show_page_button"
+  sleep(inspection_time=5)
+  expect(page).to have_select(
+                    'country',         # locator
+                    selected: 'United States' # option
+                  )
+end
+
+Given(/^I am a professional adding my profile$/) do
+  visit root_path
+  click_on "Register Your Business"
+  fill_in "email", :with => 'fashionxt133@gmail.com'
+  fill_in "password", :with => 'Abc123!'
+  fill_in "confirm", :with => 'Abc123!'
+  click_button "commit"
+
+end
+
+When(/^I am in profile addition page$/) do
+  expect(current_path).to include('general_info/new')
+end
+
+Then(/^I will see United States as the default option$/) do
+  sleep(inspection_time=5)
+  expect(page).to have_select(
+                    'country',         # locator
+                    selected: 'United States' # option
+                  )
+end
+
+
+Given(/^I am a professional editing my profile$/) do
+  visit root_path
+  click_on "Sign In"
+  fill_in "Your Email", :with => 'fashionxt102@gmail.com'
+  fill_in "Your Password", :with => 'Abc123!'
+  click_button "SIGN IN"
+  visit "/show_profile"
+  page.first('a', :text => 'Edit Profile').hover
+  sleep(inspection_time=2)
+  click_on "Edit Personal Info"
+
+end
+
+When(/^I am in profile edition page$/) do
+  expect(current_path).to include('general_info/edit')
+end
+
+Then(/^I will see United States as the default option in profile editon page$/) do
+  sleep(inspection_time=5)
+  expect(page).to have_select(
+                    'country',         # locator
+                    selected: 'United States' # option
+                  )
+end
+
+
+
+
+
 
 
 
--- features/user_story_19.feature
+++ features/user_story_19.feature
@@ -9,31 +9,26 @@ Feature:  In Search, Profile add and edition pages, US will be selected as defau
 
   Scenario: Country and city adding option in both of the search pages
     Given I am a professional and search for another professional
-    When I click on search button on home page
+    When I wish to click on search button on home page for country and city check
     Then I should be able to provide country and city information on that page
-    And And after clicking search, I can again provide country and city information on that page.
+    And after clicking search, I can again provide country and city information on that page
 
   Scenario: United states will be selected in search profile
-    Given I am a professional and search for another professional
-    When I click on search button on home page
+    Given I am a professional and intend to click the search button
+    When I intend to click the search button on home page
     Then Default country option united states should be selected
     And After submitting this search page, I can again see united states as the default contry in search result page
 
-  Scenario: United states will be selected in search profile
-    Given I am a professional and search for another professional
-    When I click on search button on home page
-    Then Default country option united states should be selected
-    And After submitting this search page, I can again see united states as the default contry in search result page
 
   Scenario: United states will be selected while adding profile
     Given I am a professional adding my profile
-    When I click on country option
+    When I am in profile addition page
     Then I will see United States as the default option
 
   Scenario: United states will be selected while editing profile
-    Given I am a professional adding my profile
-    When I click on country option
-    Then I will see United States as the default option
+    Given I am a professional editing my profile
+    When I am in profile edition page
+    Then I will see United States as the default option in profile editon page
 
 
 
