9bacb0f08ec5b24a1f1228483c324e4c55176f9e
Ahmed Tanvir Mahdad <mahdad@uab.edu>
2022-04-27 10:34:15 -0500

    Basic code added for Forgot Password Feature

---
+++ app/mailers/forgot_mailer.rb
@@ -0,0 +1,13 @@
+class ForgotMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.forgot_mailer.reset.subject
+  #
+  def reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
