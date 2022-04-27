c71fe2697736dc22559f5071199564e2e4df501a
Ahmed Tanvir Mahdad <mahdad@uab.edu>
2022-04-27 11:40:29 -0500

    Removed Some migration on Alex's commit

--- db/migrate/20220424094219_drop_messages.rb
+++
@@ -1,5 +0,0 @@
-class DropMessages < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :messages
-  end
-end
--- db/migrate/20220425024554_remove_notification_from_from_general_info.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotificationFromFromGeneralInfo < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :general_infos, :notification_from, :string
-  end
-end
