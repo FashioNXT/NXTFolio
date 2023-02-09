3ed2ebbd72062867e9aa54b8cd5a86a526be08fc
vibalcam <49537743+vibalcam@users.noreply.github.com>
2023-02-09 00:05:53 -0600

    Updated install script and dockerfile

--- install/setup.sh
+++ install/setup.sh
@@ -20,7 +20,7 @@
 # install prerequisites packages
 apt install sudo software-properties-common git -y
 # other non-necesary but useful
-apt install nano tmux
+apt install nano tmux -y
 # install rvm
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
@@ -28,7 +28,9 @@ sudo apt-get install rvm -y
 source /etc/profile.d/rvm.sh
 echo "source /etc/profile.d/rvm.sh" >> ~/.bashrc
 
-read -p "We will now install rvm and ruby \nPress [Enter] key to continue..."
+echo "We will now install rvm and ruby"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 
 # update rvm
 command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
@@ -45,7 +47,9 @@ then
     git clone "$1"
 fi
 
-read -p "We will now install postgres \nPress [Enter] key to continue..."
+echo "We will now install postgres"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 
 # install postgres
 sudo apt install -y postgresql postgresql-contrib libpq-dev -y
@@ -71,7 +75,9 @@ sudo -u postgres --  psql -c "alter user beaverthing createdb;"
 # sudo -u postgres --  psql -c "ALTER DATABASE workspace_development owner to beaverthing;"
 # sudo -u postgres --  psql -c "ALTER DATABASE workspace_test owner to beaverthing;"
 
-read -p "We will now install brew \nPress [Enter] key to continue..."
+echo "We will now install brew"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 
 # install brew (required later)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -80,7 +86,9 @@ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.bash_pro
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 
-read -p "We will now bundle app \nPress [Enter] key to continue..."
+echo "We will now bundle app"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 
 # install gem files and dependencies
 # sudo apt install libv8-dev
@@ -96,7 +104,9 @@ gem install therubyracer
 # install all gems (and hope for the best)
 bundle install
 
-read -p "We will now setup database \nPress [Enter] key to continue..."
+echo "We will now setup database"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 
 # setup tables
 source /etc/profile.d/rvm.sh
@@ -107,7 +117,9 @@ bin/rails db:seed RAILS_ENV=development
 # Start server
 # it starts in port 3000
 # if using docker with -p 8080:3000 option, you can also use localhost:8080
-read -p "All setup, initializying server on port 3000 (or localhost:8080 on host if docker) \nPress [Enter] key to continue..."
+echo "All setup, initializying server on port 3000 (or localhost:8080 on host if docker)"
+sleep 2
+# read -p "Press [Enter] key to continue..."
 rails server -b 0.0.0.0 -p 3000
 
 # if every works, that's great!
