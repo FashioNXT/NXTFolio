7746ab8915e00e0695e69287ee1406e9ba2751b1
root <root@ip-172-31-81-36.ec2.internal>
2023-02-20 05:06:04 +0000

    Working on database seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.destroy_all
+LoginInfo.destroy_all
+
+dir_path = "seed_files"
+fake_password = "Test1234!"
+
+filenames = ["Andrea-Piacquadio.jpg", "Anthony-Gray.jpg", "Jack-Window", "James-Lawrence.jpg", "Masha-Raymers.jpg", "Tasha-Washington.jpg"]
+filenames.each do |filename|
+#   puts filename
+  name = filename.gsub(".jpg", "").split("-")
+  first_name = name[0]
+  last_name = name[1]
+  LoginInfo.create!(
+    email: "#{first_name}.#{last_name}@example.com",
+    password: fake_password,
+    password_confirmation: fake_password
+  )
+  
+  puts "Creating User " + first_name + ", " + last_name
+
+end
