22b89b971d0bfeecd61e3d275e328b87aebd9afe
root <root@ip-172-31-81-36.ec2.internal>
2023-02-20 22:00:08 +0000

    Working on seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'securerandom'
 
 # User.destroy_all
 LoginInfo.destroy_all
@@ -20,23 +21,45 @@ filenames.each do |filename|
   name = filename.gsub(".jpg", "").split("-")
   first_name = name[0]
   last_name = name[1]
-  LoginInfo.create!(
-    email: "#{first_name}.#{last_name}@example.com",
-    password: fake_password,
-    password_confirmation: fake_password
-  )
-  GeneralInfo.create!(
-    first_name: first_name,
-    last_name: last_name,
-    company: "TestInc",
-    industry: "Model",
-    highlights: "Just a test User",
-    country: "United States",
-    state: "Texas",
-    city: "College Station",
-    emailaddr: "#{first_name}.#{last_name}@example.com",
-    profile_picture: File.open(Rails.root.join("db", "seed_files" ,filename))
-  )
+  # login_info = LoginInfo.create!
+  # (
+  #   email: "#{first_name}.#{last_name}@example.com",
+  #   password: fake_password,
+  #   password_confirmation: fake_password
+  # )
+  userkey = SecureRandom.hex(10)
+  login_info = LoginInfo.new
+  login_info.email = "#{first_name}.#{last_name}@example.com"
+  login_info.password = fake_password
+  login_info.password_confirmation = fake_password
+  login_info.userKey = userkey
+  login_info.save!
+  # general_info = GeneralInfo.create!(
+  #   first_name: first_name,
+  #   last_name: last_name,
+  #   company: "TestInc",
+  #   industry: "Model",
+  #   highlights: "Just a test User",
+  #   country: "United States",
+  #   state: "Texas",
+  #   city: "College Station",
+  #   emailaddr: "#{first_name}.#{last_name}@example.com",
+  #   profile_picture: File.open(Rails.root.join("db", "seed_files" ,filename))
+  # )
+
+  general_info = GeneralInfo.new
+  general_info.first_name = first_name
+  general_info.last_name = last_name
+  general_info.userKey = userkey
+  general_info.company = "TestInc"
+  general_info.industry = "Model"
+  general_info.highlights = "Just a test User"
+  general_info.country = "United States"
+  general_info.state = "Texas"
+  general_info.city = "College Station"
+  general_info.emailaddr = "#{first_name}.#{last_name}@example.com"
+  general_info.profile_picture = File.open(Rails.root.join("db", "seed_files" ,filename))
+  general_info.save!
  
   puts "Creating User " + first_name + ", " + last_name + "..."
 end
