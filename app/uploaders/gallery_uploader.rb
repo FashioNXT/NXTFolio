d1a01f628e7e9ca103d83e63cb4a1b8b628a692a
LFletch1 <ubuntu@ip-172-31-81-36.ec2.internal>
2023-03-08 18:10:56 +0000

    Trying to get S3 bucket working

--- app/uploaders/gallery_uploader.rb
+++ app/uploaders/gallery_uploader.rb
@@ -4,11 +4,11 @@ class GalleryUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.test? || Rails.env.development?
-     storage :file
-  else
-    storage :file
-  end
+  # if Rails.env.test? || Rails.env.development?
+  #    storage :file
+  # else
+  #   storage :file
+  # end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
