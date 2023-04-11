89d123128955fd546ec0c658bfae16f9446d4fe1
AJ1904 <ayushri_jain@tamu.edu>
2023-04-11 17:00:39 -0500

    working search bar for tagging

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -66,7 +66,6 @@ class GalleriesController < ApplicationController
   def create_tagging
     @gallery = Gallery.find_by(id: params[:id])
     tagged_ids = params[:gallery_tagging][:tagged_user_id].split(",").reject(&:blank?)
-    #tagged_ids = params[:gallery_tagging][:tagged_user_id].reject(&:blank?)
     tagged_ids.each do |tagged_id|
       if GalleryTagging.where(gallery_id: params[:id], general_info_id: tagged_id).empty?
         @gallery_tagging = GalleryTagging.new(gallery_id: params[:id], general_info_id: tagged_id)
@@ -84,10 +83,18 @@ class GalleriesController < ApplicationController
       project_key = params[:id]
       InvitationMailer.invitation_email(invited_email,inviter_name,project_name,project_key).deliver_now
     end
-
-    redirect_to galleries_show_path(:project_key => params[:id])
+    redirect_to '/show_profile'
+    
   end
-
+  def destroy_tagging
+    @gallery = Gallery.find(params[:gallery_id])
+    @tagging = @gallery.gallery_taggings.find(params[:id])
+    if @tagging.destroy
+      redirect_to @gallery, notice: 'Collaborator removed successfully'
+    else
+      redirect_to @gallery, alert: 'Error removing collaborator'
+    end
+  end
   
   private
 
--- app/models/gallery_tagging.rb
+++ app/models/gallery_tagging.rb
@@ -2,4 +2,10 @@ class GalleryTagging < ApplicationRecord
     belongs_to :gallery
     belongs_to :general_info
    
+    def destroy
+      @gallery_tagging = GalleryTagging.find(params[:id])
+      @gallery_tagging.destroy
+      redirect_to :back, notice: 'Collaborator removed successfully.'
+    end
+    
   end
