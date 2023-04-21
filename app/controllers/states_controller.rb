6f50ead503f4c73c2951ce4cad142e9207cc5514
Vicente Balmaseda <49537743+vibalcam@users.noreply.github.com>
2023-04-21 13:36:58 -0500

    Edit profile location and sort

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,6 +1,6 @@
 class StatesController < ApplicationController
     def index
-        @states = State.where(country_id: params[:country_id])
+        @states = State.where(country_id: params[:country_id]).order(:name)
         respond_to do |format|
             format.json { render json: @states }
         end
