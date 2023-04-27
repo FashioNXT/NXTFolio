28e63fcaab590edf66e950cbdd4bba1f47c9bcfa
Niah P <niahprice00@tamu.edu>
2023-04-27 10:14:17 -0500

    “mobilenavbar”

---
+++ features/step_definitions/mobile_navbar_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am logged in$/) do
+    visit "/login_info/logout"
+  end
+
+  When(/^I press profile.svg go to my profile$/) do
+    visit '/show_profile'
+  end
+
+  When(/^I press search.svg go to search$/) do
+    visit '/show_profile'
+  end
