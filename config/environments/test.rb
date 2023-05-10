3362a250b4a7084235996d4a717d75bd32c4d179
AJ1904 <ayushri_jain@tamu.edu>
2023-05-10 07:48:14 -0500

    update for coverage

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,13 +33,13 @@ Rails.application.configure do
   end
 
   config.cache_classes = false
-  config.action_view.cache_template_loading = false
+  config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   # config.eager_load = false
-  config.eager_load = true
+  config.eager_load = false
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
