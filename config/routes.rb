6eb09367b13505f3105f3f87cc0e025ecb3df365
Piyush Sharan <piyushsharan.ece18@iitbhu.ac.in>
2023-10-07 18:54:47 -0500

    Testing out the routes and fixing indentation

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get 'galleries/new' => 'galleries#new', :as => 'galleries/newg'
 
+  get 'galleries/private_info/:id' => 'galleries#private_info'
   
   #get 'galleries/create'
 
