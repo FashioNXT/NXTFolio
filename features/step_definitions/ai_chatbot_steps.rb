90d7c144562ac27381a74e1d601d52dc6ce51489
ghliew <liewgh1995@gmail.com>
2023-10-26 19:59:18 -0500

    updated cucumber test and steps

---
+++ features/step_definitions/ai_chatbot_steps.rb
@@ -0,0 +1,16 @@
+Given(/^I am a content creator and I want to learn how to set up fashion portfolio$/) do
+  visit root_path
+end
+
+And(/^I I want to go to the ai chatbot page$/) do
+  click_on "AI Chatbot"
+end
+
+When(/^I I enter "Hi" message and click send$/) do
+  fill_in "Type your message...", :with => "Hi"
+  click_on "Send"
+end
+
+Then(/^I should see assistant's reply$/) do
+  expect(page).to have_content("Hello! How can I assist you today?")
+end
