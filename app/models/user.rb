f6309387e297539bd377f9a8e3970dee34c3157e
ghliew <liewgh1995@gmail.com>
2023-11-22 13:36:47 -0600

    clean up fb login as it is incomplete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,30 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
+# class User < ApplicationRecord
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable and :omniauthable
+#   # devise :database_authenticatable, :registerable,
+#   #        :recoverable, :rememberable, :trackable, :validatable
 
-  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
+#   devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
-  scope :all_except, -> (user) { where.not(id: user)}
+#   scope :all_except, -> (user) { where.not(id: user)}
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
+#   def self.new_with_session(params, session)
+#     super.tap do |user|
+#       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+#         user.email = data["email"] if user.email.blank?
+#       end
+#     end
 
-  end
+#   end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the room model has a name
-      user.image = auth.info.image # assuming the room model has an image
-      #room.skip_confirmation!
-    end
-  end
+#   def self.from_omniauth(auth)
+#     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+#       user.email = auth.info.email
+#       user.password = Devise.friendly_token[0,20]
+#       user.name = auth.info.name   # assuming the room model has a name
+#       user.image = auth.info.image # assuming the room model has an image
+#       #room.skip_confirmation!
+#     end
+#   end
 
-end
+# end
