fa765b3ce7311b93c0a511370a1f45add0acd904
ghliew <liewgh1995@gmail.com>
2023-11-21 21:53:28 -0600

    forgot_mailer

---
+++ features/forgot_mailer.feature
@@ -0,0 +1,5 @@
+Feature: Forgot and Reset Password
+
+  Scenario: I forgot my password and want to reset
+    Given I want to reset my password
+    When I fill in the email
---
+++ features/step_definitions/forgot_mailer_steps.rb
@@ -0,0 +1,8 @@
+Given(/^I want to reset my password$/) do
+  visit "/password/reset"
+end
+
+When(/^I fill in the email$/) do
+  fill_in "example@nxtfolio.com", :with => "test123@gmail.com"
+  ForgotMailer.reset(test123@gmail.com).deliver_now
+end
