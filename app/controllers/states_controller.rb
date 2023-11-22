f57a83ba2df46af98bd7d24b14fab4d328d69750
ghliew <liewgh1995@gmail.com>
2023-11-21 20:30:42 -0600

    legacy code cleanup

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,8 +1,8 @@
-class StatesController < ApplicationController
-    def index
-        @states = State.where(country_id: params[:country_id]).order(:name)
-        respond_to do |format|
-            format.json { render json: @states }
-        end
-    end
-end
+# class StatesController < ApplicationController
+#     def index
+#         @states = State.where(country_id: params[:country_id]).order(:name)
+#         respond_to do |format|
+#             format.json { render json: @states }
+#         end
+#     end
+# end
