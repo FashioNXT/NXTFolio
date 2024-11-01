3098bdd5e4ecaf365509e1532e186764d31e2def
BranHill21 <bhill20@austincollege.edu>
2024-11-01 09:58:38 -0500

    refactored applicationjs to remove all code smells

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -78,137 +78,38 @@ $( document ).ready(function() {
     hideAll();
     console.log("Printing Button Status:..")
 
-    $(".modelbutton").on("click", function(){
-        localStorage.setItem('buttonpressed', 'model');
-        cleanButtonDiv();
-        $(".modeldiv").show();
-        $(".modelbutton").addClass("active");
-    });
-    $(".brandbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'brand');
-        cleanButtonDiv();
-        $(".branddiv").show();
-        $(".brandbutton").addClass("active");
-    });
-
-    $(".photographerbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'photographer');
-        cleanButtonDiv();
-        $(".photographerdiv").show();
-        $(".photographerbutton").addClass("active");
-    });
-
-    $(".designerbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'designer');
-        cleanButtonDiv();
-        $(".designerdiv").show();
-        $(".designerbutton").addClass("active");
-    });
-
-    $(".othercreatorbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'othercreator');
-        cleanButtonDiv();
-        $(".othercreatordiv").show();
-        $(".othercreatorbutton").addClass("active");
-    });
-
-    $(".influencerbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'influencer');
-        cleanButtonDiv();
-        $(".influencerdiv").show();
-        $(".influencerbutton").addClass("active");
-    });
-
-    $(".contentbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'content');
-        cleanButtonDiv();
-        $(".contentdiv").show();
-        $(".contentbutton").addClass("active");
-    });
-
-    $(".salesbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'sales');
-        cleanButtonDiv();
-        $(".salesdiv").show();
-        $(".salesbutton").addClass("active");
-    });
-
-    $(".bloggerbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'blogger');
-        cleanButtonDiv();
-        $(".bloggingdiv").show();
-        $(".bloggerbutton").addClass("active");
-    });
-
-    $(".marketingbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'marketing');
-        cleanButtonDiv();
-        $(".marketingdiv").show();
-        $(".marketingbutton").addClass("active");
-    });
-
-    $(".retailbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'retail');
-        cleanButtonDiv();
-        $(".retaildiv").show();
-        $(".retailbutton").addClass("active");
-    });
-
-    $(".visualbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'visual');
-        cleanButtonDiv();
-        $(".visualdiv").show();
-        $(".visualbutton").addClass("active");
-    });
-
-    $(".financingbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'financing');
-        cleanButtonDiv();
-        $(".financingdiv").show();
-        $(".financingbutton").addClass("active");
-    });
-
-    $(".forecastingbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'forecasting');
-        cleanButtonDiv();
-        $(".forecastingdiv").show();
-        $(".forecastingbutton").addClass("active");
-    });
-
-    $(".stylistbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'stylist');
-        cleanButtonDiv();
-        $(".stylistdiv").show();
-        $(".stylistbutton").addClass("active");
-    });
-
-    $(".otherservicesbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'otherservices');
-        cleanButtonDiv();
-        $(".otherservicesdiv").show();
-        $(".otherservicesbutton").addClass("active");
-    });
-
-    $(".manufacturingbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'manufacturing');
-        cleanButtonDiv();
-        $(".manufacturingdiv").show();
-        $(".manufacturingbutton").addClass("active");
-    });
-
-    $(".materialsbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'materials');
-        cleanButtonDiv();
-        $(".materialsdiv").show();
-        $(".materialsbutton").addClass("active");
-    });
-
-    $(".othermakersbutton").click(function () {
-        localStorage.setItem('buttonpressed', 'othermakers');
-        cleanButtonDiv();
-        $(".othermakersiv").show();
-        $(".othermakersbutton").addClass("active");
-    });
+    function setupButton(buttonType) {
+        $('.${buttonType}button').click(function () {
+            localStorage.setItem('buttonpressed', buttonType);
+            hideAll();
+            deactivateAll();
+            $('.${buttonType}div').show();
+            $('.${buttonType}button').addClass("active");
+        });
+    }
+    
+    // Call setupButton for each button type
+    setupButton("brand");
+    setupButton("photographer");
+    setupButton("designer");
+    setupButton("othercreator");
+    setupButton("influencer");
+    setupButton("content");
+    setupButton("sales");
+    setupButton("blogger");
+    setupButton("marketing");
+    setupButton("retail");
+    setupButton("financing");
+    setupButton("otherservices");
+    setupButton("manufacturing");
+    setupButton("materials");
+    setupButton("othermakers");
+    setupButton("visual");
+    setupButton("model");
+    setupButton("agent");
+    setupButton("everyone");
+    setupButton("stylist");
+    setupButton("forecasting");
 });
 
 
