3de3fe2df5cbce9d6742a288398dc04b63d1910c
Yash Honrao <yashhonrao2024@gmail.com>
2024-11-19 19:05:16 -0600

    Tests for RoomsHelper

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,7 @@
 module RoomsHelper
+    def format_room_number(number)
+        "Room #{number.to_s.rjust(3, '0')}"
+      end
+    
+     
 end
--- spec/helpers/rooms_helper_spec.rb
+++ spec/helpers/rooms_helper_spec.rb
@@ -11,5 +11,20 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe RoomsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "#format_room_number" do
+    it "formats room number with leading zeros" do
+      expect(helper.format_room_number(5)).to eq("Room 005")
+    end
+
+    it "handles double-digit room numbers" do
+      expect(helper.format_room_number(42)).to eq("Room 042")
+    end
+
+    it "handles triple-digit room numbers" do
+      expect(helper.format_room_number(100)).to eq("Room 100")
+    end
+  end
+
+ 
 end
