6cadc7129cf9cce740b9aabd88e2d1d61c05ba71
Omar Khater <omarkhater@tamu.edu>
2024-11-19 22:13:42 -0600

    Update registrations_controller_spec.rb
    - Better Code indentation
    - Add some tests with an already registered email

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,54 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
-  include Devise::Test::ControllerHelpers  
+    include Devise::Test::ControllerHelpers  
 
-  before do
-    @request.env["devise.mapping"] = Devise.mappings[:user] # Setup Devise mapping
-  end
-
-  describe 'GET #new' do
-    subject { get :new }
-
-    it 'renders the new template' do
-      subject
-      expect(response).to render_template(:new)
+    before do
+        @request.env["devise.mapping"] = Devise.mappings[:user] # Setup Devise mapping
     end
 
-    it 'responds with a 200 status code' do
-      subject
-      expect(response).to have_http_status(:ok)
-    end
-  end
+    describe 'GET #new' do
+        subject { get :new }
 
-  describe 'POST #create' do
-    let(:user_params) { { email: 'test@example.com', password: 'Test1234!', password_confirmation: 'Test1234!' } }
-
-    context 'with valid parameters' do
-      subject { post :create, params: { user: user_params } }
-
-      it 'does not save the user to the database yet' do
-        expect { subject }.not_to change(User, :count)
-      end
-
-      it 'redirects to the general_info/new path' do
+        it 'renders the new template' do
         subject
-        expect(response).to redirect_to(general_info_new_path)
-      end
+        expect(response).to render_template(:new)
+        end
+
+        it 'responds with a 200 status code' do
+        subject
+        expect(response).to have_http_status(:ok)
+        end
     end
 
-    context 'with invalid parameters' do
-        it 'does not create a new user and re-renders new' do
-          expect {
+    describe 'POST #create' do
+        let(:user_params) { { email: 'test@example.com', password: 'Test1234!', password_confirmation: 'Test1234!' } }
+
+        context 'with valid parameters' do
+        subject { post :create, params: { user: user_params } }
+
+        it 'does not save the user to the database yet' do
+            expect { subject }.not_to change(User, :count)
+        end
+
+        it 'redirects to the general_info/new path' do
+            subject
+            expect(response).to redirect_to(general_info_new_path)
+        end
+        end
+
+        context 'with invalid parameters' do
+            it 'does not create a new user and re-renders new' do
+            expect {
+                post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
+            }.not_to change(User, :count)
+            expect(response).to render_template(:new)
+            end
+    
+            it 'sets flash error messages' do
             post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
-          }.not_to change(User, :count)
-          expect(response).to render_template(:new)
+            expect(flash[:error]).to be_present
+            end
         end
-  
-        it 'sets flash error messages' do
-          post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
-          expect(flash[:error]).to be_present
+        context 'with an already registered email' do
+            before { User.create!(user_params) }
+    
+            it 'does not create a new user and re-renders new' do
+            expect {
+                post :create, params: { user: user_params }
+            }.not_to change(User, :count)
+            expect(response).to render_template(:new)
+            end
+    
+            it 'sets a flash error for duplicate email' do
+            post :create, params: { user: user_params }
+            expect(flash[:error]).to eq('Email has already been taken')
+            end
         end
-      end
-  end
+    end
 end
