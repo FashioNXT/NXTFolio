48888abd8944e69bf373dc9345662e9a65e6ba0e
Omar Khater <omarkhater@tamu.edu>
2024-11-19 22:27:01 -0600

    Update registrations_controller_spec.rb
    - Better Indentation
    - More test cases when the user is invalid and save is not invoked

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe Users::RegistrationsController, type: :controller do
         subject { get :new }
 
         it 'renders the new template' do
-        subject
-        expect(response).to render_template(:new)
+            subject
+            expect(response).to render_template(:new)
         end
 
         it 'responds with a 200 status code' do
-        subject
-        expect(response).to have_http_status(:ok)
+            subject
+            expect(response).to have_http_status(:ok)
         end
     end
 
@@ -25,45 +25,69 @@ RSpec.describe Users::RegistrationsController, type: :controller do
         let(:user_params) { { email: 'test@example.com', password: 'Test1234!', password_confirmation: 'Test1234!' } }
 
         context 'with valid parameters' do
-        subject { post :create, params: { user: user_params } }
+            subject { post :create, params: { user: user_params } }
 
-        it 'does not save the user to the database yet' do
-            expect { subject }.not_to change(User, :count)
-        end
+            it 'does not save the user to the database yet' do
+                expect { subject }.not_to change(User, :count)
+            end
 
-        it 'redirects to the general_info/new path' do
-            subject
-            expect(response).to redirect_to(general_info_new_path)
-        end
+            it 'redirects to the general_info/new path' do
+                subject
+                expect(response).to redirect_to(general_info_new_path)
+            end
         end
 
         context 'with invalid parameters' do
             it 'does not create a new user and re-renders new' do
-            expect {
-                post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
-            }.not_to change(User, :count)
-            expect(response).to render_template(:new)
+                expect {
+                    post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
+                }.not_to change(User, :count)
+                expect(response).to render_template(:new)
             end
     
             it 'sets flash error messages' do
-            post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
-            expect(flash[:error]).to be_present
+                post :create, params: { user: user_params.merge(password_confirmation: 'WrongPassword') }
+                expect(flash[:error]).to be_present
             end
         end
+
         context 'with an already registered email' do
             before { User.create!(user_params) }
     
             it 'does not create a new user and re-renders new' do
-            expect {
-                post :create, params: { user: user_params }
-            }.not_to change(User, :count)
-            expect(response).to render_template(:new)
+                expect {
+                    post :create, params: { user: user_params }
+                }.not_to change(User, :count)
+                expect(response).to render_template(:new)
             end
     
             it 'sets a flash error for duplicate email' do
-            post :create, params: { user: user_params }
-            expect(flash[:error]).to eq('Email has already been taken')
+                post :create, params: { user: user_params }
+                expect(flash[:error]).to eq('Email has already been taken')
             end
         end
+
+        context 'when the user is invalid and save is not invoked' do
+            before do
+              # Simulate validation failure by making the user invalid
+              allow_any_instance_of(User).to receive(:valid?).and_return(false)
+            end
+          
+            it 'does not create a new user' do
+              expect {
+                post :create, params: { user: user_params }
+              }.not_to change(User, :count)
+            end
+          
+            it 're-renders the new template' do
+              post :create, params: { user: user_params }
+              expect(response).to render_template(:new)
+            end
+          
+            it 'sets flash error messages' do
+              post :create, params: { user: user_params }
+              expect(flash[:error]).to be_present
+            end
+          end
     end
 end
