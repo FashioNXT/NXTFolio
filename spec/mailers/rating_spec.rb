9cc9ded7fd8bf2d121906eefac5d49cf46932e36
Yash Honrao <yashhonrao2024@gmail.com>
2024-11-19 19:56:31 -0600

    RSpec test for Rating Mailer

--- spec/mailers/rating_spec.rb
+++ spec/mailers/rating_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe RatingMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "gallery_rating_email" do
+    let(:email) { "owner@example.com" }
+    let(:owner_name) { "Jane Smith" }
+    let(:reviewer_name) { "John Doe" }
+    let(:project_name) { "Summer Fashion Collection" }
+    let(:rating) { 4 }
+    let(:mail) { RatingMailer.gallery_rating_email(email, owner_name, reviewer_name, project_name, rating) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq('One of your projects has been rated')
+      expect(mail.to).to eq([email])
+      expect(mail.from).to eq(["fashionxtllc@gmail.com"]) # Adjust if your default from address is different
+    end
+
+    
+  end
 end
