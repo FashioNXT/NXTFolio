1bf08e930a3fe6dfdbf21a2df97c49af7517570c
Yash Honrao <yashhonrao2024@gmail.com>
2024-11-19 19:42:02 -0600

    RSpec test for Job_notification_spec

--- spec/mailers/job_notification_spec.rb
+++ spec/mailers/job_notification_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe JobNotificationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "job_notification_email" do
+    let(:email) { "john@example.com" }
+    let(:name) { "John Doe" }
+    let(:job) do
+      double("Job", 
+        id: 1,
+        title: "Fashion Photographer"
+      )
+    end
+    let(:mail) { JobNotificationMailer.job_notification_email(email, name, job) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("New job you may be interested in")
+      expect(mail.to).to eq([email])
+      expect(mail.from).to eq(["fashionxtllc@gmail.com"]) 
+    end
+
+    
+  end
 end
