50f5a8be28456e16baf9c457b71832f2ff0487cd
Yash Honrao <yashhonrao2024@gmail.com>
2024-11-21 14:41:56 -0600

    Tests for user_activity_detail model

--- spec/models/user_activity_detail_spec.rb
+++ spec/models/user_activity_detail_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UserActivityDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "scopes" do
+    before do
+      @activity1 = UserActivityDetail.create(created_at: 2.days.ago)
+      @activity2 = UserActivityDetail.create(created_at: 1.day.ago)
+    end
+
+    it "orders activities by most recent first" do
+      expect(UserActivityDetail.recent).to eq([@activity2, @activity1])
+    end
+  end
 end
